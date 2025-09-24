d1cdd81948eaf08d401a8517bbcc579e3c83f4f8
flejz <flejz@protonmail.com>
2025-09-24 13:43:37 +0200

    feat: add fidget for lsp updates

--- nvim/init-win.lua
+++ nvim/init-win.lua
@@ -23,6 +23,7 @@ vim.opt.smarttab = true
 vim.opt.foldmethod = 'indent'
 vim.opt.foldnestmax = 10
 vim.opt.foldlevel = 1
+vim.opt.foldenable = false
 vim.opt.formatoptions:append({ 'tcqrn1' })
 -- vim.opt.guitablabel = '%M%t'
 vim.opt.hidden = true
@@ -129,6 +130,7 @@ require("lazy").setup({
   { "sheerun/vim-polyglot" },
   { "nvim-treesitter/nvim-treesitter",  build = ":TSUpdate" },
   { "nvim-lua/plenary.nvim" },
+  { "j-hui/fidget.nvim" },
 
   -- dap (debugging)
   { "mfussenegger/nvim-dap" },
@@ -204,7 +206,7 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 
 -- color scheme
 vim.cmd("colorscheme vscode")
-vim.api.nvim_set_hl(0, "LspInlayHint", { fg="#A3A3A3" })
+vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#A3A3A3" })
 
 -- enable syntax highlighting.
 vim.cmd("filetype plugin indent on")
@@ -408,20 +410,131 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
--- load telescope
+-- fidget
+local fidget = require("fidget")
+fidget.setup({
+  -- Options related to LSP progress subsystem
+  progress = {
+    poll_rate = 0,                -- How and when to poll for progress messages
+    suppress_on_insert = false,   -- Suppress new messages while in insert mode
+    ignore_done_already = false,  -- Ignore new tasks that are already complete
+    ignore_empty_message = false, -- Ignore new tasks that don't contain a message
+    clear_on_detach =             -- Clear notification group when LSP server detaches
+        function(client_id)
+          local client = vim.lsp.get_client_by_id(client_id)
+          return client and client.name or nil
+        end,
+    notification_group = -- How to get a progress message's notification group key
+        function(msg) return msg.lsp_client.name end,
+    ignore = {},         -- List of LSP servers to ignore
+
+    -- Options related to how LSP progress messages are displayed as notifications
+    display = {
+      render_limit = 16, -- How many LSP messages to show at once
+      done_ttl = 3, -- How long a message should persist after completion
+      done_icon = "✔", -- Icon shown when all LSP progress tasks are complete
+      done_style = "Constant", -- Highlight group for completed LSP tasks
+      progress_ttl = math.huge, -- How long a message should persist when in progress
+      progress_icon = -- Icon shown when LSP progress tasks are in progress
+      { "dots" },
+      progress_style = -- Highlight group for in-progress LSP tasks
+      "WarningMsg",
+      group_style = "Title", -- Highlight group for group name (LSP server name)
+      icon_style = "Question", -- Highlight group for group icons
+      priority = 30, -- Ordering priority for LSP notification group
+      skip_history = true, -- Whether progress notifications should be omitted from history
+      format_message = -- How to format a progress message
+          require("fidget.progress.display").default_format_message,
+      format_annote = -- How to format a progress annotation
+          function(msg) return msg.title end,
+      format_group_name = -- How to format a progress notification group's name
+          function(group) return tostring(group) end,
+      overrides = { -- Override options from the default notification config
+        rust_analyzer = { name = "rust-analyzer" },
+      },
+    },
+
+    -- Options related to Neovim's built-in LSP client
+    lsp = {
+      progress_ringbuf_size = 0, -- Configure the nvim's LSP progress ring buffer size
+      log_handler = false,       -- Log `$/progress` handler invocations (for debugging)
+    },
+  },
+
+  -- Options related to notification subsystem
+  notification = {
+    poll_rate = 10,               -- How frequently to update and render notifications
+    filter = vim.log.levels.INFO, -- Minimum notifications level
+    history_size = 128,           -- Number of removed messages to retain in history
+    override_vim_notify = false,  -- Automatically override vim.notify() with Fidget
+    configs =                     -- How to configure notification groups when instantiated
+    { default = require("fidget.notification").default_config },
+    redirect =                    -- Conditionally redirect notifications to another backend
+        function(msg, level, opts)
+          if opts and opts.on_open then
+            return require("fidget.integration.nvim-notify").delegate(msg, level, opts)
+          end
+        end,
+
+    -- Options related to how notifications are rendered as text
+    view = {
+      stack_upwards = true,    -- Display notification items from bottom to top
+      align = "message",       -- Indent messages longer than a single line
+      reflow = false,          -- Reflow (wrap) messages wider than notification window
+      icon_separator = " ",    -- Separator between group name and icon
+      group_separator = "---", -- Separator between notification groups
+      group_separator_hl =     -- Highlight group used for group separator
+      "Comment",
+      line_margin = 1,         -- Spaces to pad both sides of each non-empty line
+      render_message =         -- How to render notification messages
+          function(msg, cnt)
+            return cnt == 1 and msg or string.format("(%dx) %s", cnt, msg)
+          end,
+    },
+
+    -- Options related to the notification window and buffer
+    window = {
+      normal_hl = "Comment", -- Base highlight group in the notification window
+      winblend = 100,        -- Background color opacity in the notification window
+      border = "none",       -- Border around the notification window
+      zindex = 45,           -- Stacking priority of the notification window
+      max_width = 0,         -- Maximum width of the notification window
+      max_height = 0,        -- Maximum height of the notification window
+      x_padding = 1,         -- Padding from right edge of window boundary
+      y_padding = 0,         -- Padding from bottom edge of window boundary
+      align = "bottom",      -- How to align the notification window
+      relative = "editor",   -- What the notification window position is relative to
+      tabstop = 8,           -- Width of each tab character in the notification window
+      avoid = {}             -- Filetypes the notification window should avoid
+      -- e.g., { "aerial", "NvimTree", "neotest-summary" }
+    },
+  },
+
+  -- Options related to integrating with other plugins
+  integration = {
+    ["nvim-tree"] = {
+      enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
+      -- DEPRECATED; use notification.window.avoid = { "NvimTree" }
+    },
+    ["xcodebuild-nvim"] = {
+      enable = true, -- Integrate with wojciech-kulik/xcodebuild.nvim (if installed)
+      -- DEPRECATED; use notification.window.avoid = { "TestExplorer" }
+    },
+  },
+
+  -- Options related to logging
+  logger = {
+    level = vim.log.levels.WARN, -- Minimum logging level
+    max_size = 10000,            -- Maximum log file size, in KB
+    float_precision = 0.01,      -- Limit the number of decimals displayed for floats
+    path =                       -- Where Fidget writes its logs to
+        string.format("%s/fidget.nvim.log", vim.fn.stdpath("cache")),
+  },
+})
+
+
+-- telescope
 local telescope = require('telescope')
-
-local function is_telescope_open()
-  local wins = vim.api.nvim_list_wins()
-  for _, win in ipairs(wins) do
-    local buf = vim.api.nvim_win_get_buf(win)
-    if vim.api.nvim_buf_get_option(buf, 'filetype') == 'TelescopePrompt' then
-      return true
-    end
-  end
-  return false
-end
-
 local actions = require('telescope.actions')
 local action_layout = require("telescope.actions.layout")
 
@@ -443,6 +556,7 @@ telescope.setup {
     },
   },
 }
+-- telescope.load_extension("fidget")
 
 vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>lua require("telescope.builtin").find_files()<CR>',
   { noremap = true, silent = true })
@@ -454,6 +568,7 @@ vim.api.nvim_set_keymap('n', '<C-f>h', '<cmd>lua require("telescope.builtin").he
   { noremap = true, silent = true })
 
 -- lualine
+
 local lualine = require('lualine')
 lualine.setup {
   options = {
